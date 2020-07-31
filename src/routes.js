ae05b2c468bc0aec68b0c2c4af59a23c9d045a8f
priyansh <pp.3535.pp@gmail.com>
2020-07-31 15:06:59 +0530

    company page add

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ const Company = React.lazy(() => import('./views/company/Company'));
 
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
   { path: '/create-company', name: 'Company', component: Company },
 ];
 
